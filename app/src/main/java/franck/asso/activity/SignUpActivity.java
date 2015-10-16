da1b9015c6bbc220e5a66ba2e2be0ef31b0bc472
FranckW <franck.warlouzet@hotmail.fr>
2015-10-16 13:01:33 +0200

    Add sign up page

--- app/src/main/java/franck/asso/activity/SignUpActivity.java
+++ app/src/main/java/franck/asso/activity/SignUpActivity.java
@@ -2,6 +2,8 @@ package franck.asso.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 import franck.asso.R;
 
@@ -14,5 +16,15 @@ public class SignUpActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void onGenderButtonClicked(View view) {
+        RadioButton maleButton = (RadioButton) findViewById(R.id.radioButtonMale);
+        RadioButton femaleButton = (RadioButton) findViewById(R.id.radioButtonFemale);
+        if (femaleButton.isPressed()) {
+            maleButton.setChecked(false);
+        } else if (maleButton.isPressed()) {
+            femaleButton.setChecked(false);
+        }
     }
 }
